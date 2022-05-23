300d352d184afcc5bbcc7eea7f94fdfb8937ce93
BlackBoltEdu <jeff.rib10e@gmail.com>
2022-05-22 23:13:32 -0300

    80% landing Page

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.scrollReveal', {duration: 1000});
+
+sr.reveal('.scrollReveal-left', {
+    origin: 'left',
+    distance: '50px',
+    duration: 2500
+});
+
+sr.reveal('.scrollReveal-right', {
+    origin: 'right',
+    distance: '50px',
+    duration: 2500
+});
+
+sr.reveal('.scrollReveal-bottom', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 2500
+});
