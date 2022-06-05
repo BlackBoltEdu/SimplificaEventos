66c6ae76893e23ed5006ee03e4eca7c2b1dc5e0f
BlackBoltEdu <jeff.rib10e@gmail.com>
2022-06-05 05:06:29 -0300

    Desespero

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,27 @@ services.forEach((service) => {
         });
     });
 });
+
+const itensSelecionados = document.querySelectorAll('.input-agenda input[type="checkbox"]');
+const btnAgendar = document.querySelector('#btn-agendar');
+let arrayServicos = [];
+
+document.querySelectorAll('.input-agenda input').forEach(function(input) {
+    input.addEventListener('change', function() {
+        this.parentNode.classList.toggle('checked-agenda');
+    });
+});
+
+btnAgendar.addEventListener('click', (e) => {
+    itensSelecionados.forEach((item) => {
+        if (item.checked) {
+            if(arrayServicos.indexOf(item.value) === -1){
+                arrayServicos.splice(item.value !== true);
+                arrayServicos.push(item.value);
+                
+                console.log(arrayServicos);
+            }           
+        }
+    })
+    e.preventDefault();
+})
